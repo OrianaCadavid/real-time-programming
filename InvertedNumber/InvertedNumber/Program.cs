56b7629f4219d609a59c12efb31ecba22bb7bd03
OrianaCadavid <ocadavid@utp.edu.cp>
2017-12-12 20:06:13 +0100

    A console application that changes the order of a two-digit number

---
+++ InvertedNumber/InvertedNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvertedNumber
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			int number1 = 0;
+			int number2 = 0;
+			Console.WriteLine("Enter two numbers");
+			number1 = Convert.ToInt32(Console.ReadLine());
+			number2 = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine(Convert.ToString(number2) + number1);
+
+			Console.ReadKey();
+		}
+	}
+}
