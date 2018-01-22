05b2320ff6b7d1713648568e524aa8ccdf1a6e13
OrianaCadavid <ocadavid@utp.edu.cp>
2018-01-22 20:16:12 +0100

    A console application that show a list of employees

---
+++ ListPersons/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ListPersons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int employees = 0;
+
+            Console.WriteLine("Enter the number of employees");
+            employees = Convert.ToInt32(Console.ReadLine());
+
+            string[] names = new string[employees];
+            int[] age = new int[employees];
+
+
+
+            for (int i = 0; i < employees; i++)
+            {
+                Console.WriteLine("Enter the name and then the age of the employees");
+                names[i] = Console.ReadLine();
+                age[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Employees are:");
+            for (int i = 0; i < employees; i++)
+            {
+
+                Console.WriteLine(names[i] + "   ");
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
